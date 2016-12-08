505b955f1b2a69326364eb7b8c7d8fd85fbc78d2
Marty Hernandez Avedon <martyavedon@gmail.com>
2016-12-08 16:27:23 -0500

    PICS.NOW. (but in the wrong place??)

--- AC3.2-MidtermElements/AC3.2-MidtermElements/PeriodicTableViewController.swift
+++ AC3.2-MidtermElements/AC3.2-MidtermElements/PeriodicTableViewController.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 
-class PeriodicTableViewController: UITableViewController { 
-
+class PeriodicTableViewController: UITableViewController {
+    
     let getString = "https://api.fieldbook.com/v1/58488d40b3e2ba03002df662/elements"
     let postString = "https://api.fieldbook.com/v1/58488d40b3e2ba03002df662/favorites"
     
@@ -44,16 +44,16 @@ class PeriodicTableViewController: UITableViewController {
          }
          }
          }
-        */
-
+         */
+        
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let numOfCells = elements?.count ?? 0
         return numOfCells
@@ -68,27 +68,49 @@ class PeriodicTableViewController: UITableViewController {
         // set the name
         cell.textLabel?.text = thisParticularElement?.name
         // set the subtitle
-        cell.detailTextLabel?.text = "\(thisParticularElement?.symbol)(\(thisParticularElement?.number)) \(thisParticularElement?.weight)"
+        if let unwrappedSymbol = thisParticularElement?.symbol,
+            let unwrappedNumber = thisParticularElement?.number,
+            let unwrappedWeight = thisParticularElement?.weight {
+            cell.detailTextLabel?.text = "\(unwrappedSymbol)(\(unwrappedNumber)) \(unwrappedWeight)"
+        }
+            
         // reset the image to nil
         cell.imageView?.image = nil
         
         // make the call to get the correct image
-//            APIRequestManager.manager.getData(endPoint:  ) { (data: Data?) in
-//                if  let validData = data,
-//                    let validImage = UIImage(data: validData) {
-//                    DispatchQueue.main.async {
-//                        cell.imageView?.image = validImage
-//                        cell.setNeedsLayout()
-//                    }
-//                }
-//            }
-//
+        APIRequestManager.manager.getData(endPoint: "https://s3.amazonaws.com/ac3.2-elements/" + "\(thisParticularElement!.symbol)" + thumbSuffix) { (data: Data?) in
+            if  let validData = data,
+                let validImage = UIImage(data: validData) {
+                DispatchQueue.main.async {
+                    cell.imageView?.image = validImage
+                    cell.setNeedsLayout()
+                }
+            }
+        }
+        
+        /*
+         func downloadImage(url: URL) {
+         print("Download Started")
+         getDataFrom(url: url) { (data, response, error)  in
+         guard let data = data, error == nil else { return }
+         print(response?.suggestedFilename ?? url.lastPathComponent)
+         print("Download Finished")
+         DispatchQueue.main.async() { () -> Void in
+         // set a remote image for a normal image view
+         self.photo.image = UIImage(data: data)
+         // set a remote image for the background of a button -- this is useful for when we want to make clickable images, because making image views clickable is kind of a hack and not good for screen readers
+         self.clickableImg.setBackgroundImage(UIImage(data: data), for: .normal)
+         }
+         }
+         }
+        */
+        
         return cell
     }
-
+    
     
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard segue.identifier == "cellToDetail" else { return }
