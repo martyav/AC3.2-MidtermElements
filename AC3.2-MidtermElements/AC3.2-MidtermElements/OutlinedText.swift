c7164c814cbc7d78d453fac457d353925e383052
Marty Hernandez Avedon <martyavedon@C4Qs-MacBook-Pro-72.local>
2016-12-10 20:17:05 -0500

    outlined text & changes to detail view coloring

--- AC3.2-MidtermElements/AC3.2-MidtermElements/OutlinedText.swift
+++ AC3.2-MidtermElements/AC3.2-MidtermElements/OutlinedText.swift
@@ -6,4 +6,23 @@
 //  Copyright © 2016 Marty Hernandez Avedon. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+// from http://stackoverflow.com/questions/1103148/how-do-i-make-uilabel-display-outlined-text
+
+class UIOutlinedLabel: UILabel {
+    
+    var outlineWidth: CGFloat = 1
+    var outlineColor: UIColor = .white
+    
+    override func drawText(in rect: CGRect) {
+        
+        let strokeTextAttributes = [
+            NSStrokeColorAttributeName : outlineColor,
+            NSStrokeWidthAttributeName : -1 * outlineWidth, // making this negative gives a border around filled text; postive gives outlined text with a transparent fill
+            ] as [String : Any]
+        
+        self.attributedText = NSAttributedString(string: self.text ?? "", attributes: strokeTextAttributes)
+        super.drawText(in: rect)
+    }
+}
