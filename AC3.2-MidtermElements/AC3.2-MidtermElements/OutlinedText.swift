817fb27ffeb32c5ff0b218a1c172dc49b5115485
Marty Hernandez Avedon <martyavedon@C4Qs-MacBook-Pro-72.local>
2016-12-10 20:00:41 -0500

    added dead-simple loading message, altered detail to resemble traditional periodic table cell

---
+++ AC3.2-MidtermElements/AC3.2-MidtermElements/OutlinedText.swift
@@ -0,0 +1,9 @@
+//
+//  OutlinedText.swift
+//  AC3.2-MidtermElements
+//
+//  Created by Marty Avedon on 12/10/16.
+//  Copyright © 2016 Marty Hernandez Avedon. All rights reserved.
+//
+
+import Foundation
