637715e1a57b84ca7ebdadd3e3ab5bfa4e0c603a
Marty Hernandez Avedon <martyavedon@gmail.com>
2016-12-12 15:12:21 -0500

    fixed oversight in how json coded up lathanide/actinide series metals

---
+++ AC3.2-MidtermElements/Pods/Target Support Files/Pods-AC3.2-MidtermElements/Pods-AC3.2-MidtermElements-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_AC3_2_MidtermElementsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_AC3_2_MidtermElementsVersionString[];
+
